3338fe1ef8d78eea293a171091278fffa18a78e2
Tymoteusz <tymoteusz.stepien@wp.pl>
2019-05-02 19:14:58 +0200

    [binary-tree]changed logic a little bit

--- binary-tree/tree.js
+++ binary-tree/tree.js
@@ -1,23 +1,35 @@
-class Root {
-    constructor(val) {
+class Node {
+    constructor(val, index) {
         this.number = val || null;
         this.left = null;
         this.right = null;
+        this.index = index || null;
     }
-    addValue(val) {
-        if(this.number == null) this.number = val;
-        if (val < this.number) {
-            this.left = new Root(val);
-        } else if (val > this.number) {
-            this.right = new Root(val);
+}
+
+class Tree {
+    constructor() {
+        this.tree = new Node();
+    }
+    addValue({value,index}) {
+        if(index == 0){
+            this.tree.index = index;
+            this.tree.number = value;
+        }
+        else {
+            if(value < this.tree.number) this.tree.left = new Node(value,index);
+            if(value > this.tree.number) this.tree.right = new Node(value,index);
         }
     }
 }
 
-let tree = new Root();
+let tree = new Tree();
 
 for(let i = 0; i<15; i++){
-    tree.addValue(Math.floor(Math.random()*101));
+    tree.addValue({
+        "value": Math.floor(Math.random()*101),
+        "index": i
+    });
 }
 
-console.log(tree);
+console.log(tree.tree);

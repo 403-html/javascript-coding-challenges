a12fc97048857289be1da403e0bd72279c91a6fc
403-html <tymoteusz.stepien@wp.pl>
2020-11-23 21:17:50 +0100

    [JCC 4] Add pigeons tests

--- pigeons-challenge/pigeons.js
+++ pigeons-challenge/pigeons.js
@@ -1,23 +1,14 @@
-function feeding(totalWheat = 0){
-    let pigeonsTotal = 0;
-    let nextPigeonGroup = 1;
-    let currWheat = totalWheat;
-    while(pigeonsTotal < currWheat) {
-        if(pigeonsTotal+nextPigeonGroup>totalWheat) break;
-        pigeonsTotal += nextPigeonGroup;
-        currWheat -= pigeonsTotal;
-        nextPigeonGroup++;
+const feeding = (totalWheat = 0) => {
+    let pigeonsTotal = 0
+    let nextPigeonGroup = 1
+    let currWheat = totalWheat
+    while (pigeonsTotal < currWheat) {
+        if (pigeonsTotal + nextPigeonGroup > totalWheat) break
+        pigeonsTotal += nextPigeonGroup
+        currWheat -= pigeonsTotal
+        nextPigeonGroup++
     }
-    return console.log(`With ${totalWheat} wheat you can feed ${pigeonsTotal} pigeons - last group of pigeons is equal to ${nextPigeonGroup} `)
+    return pigeonsTotal
 }
 
-
-// 1 pigeon - 1 wheat
-// total wheat 10 - 1 = 9
-// 2 next pigeons group + 1 curr pigeon = 3 pigeons - 3 wheat
-// total wheat 9 - 3 = 6
-// 3 next pigeons group + 3 curr pigeons = 6 pigeons - 6 wheat
-// total wheat 6 - 6 = 0
-// total wheat < pigeonsTotal = break
-
-feeding(10);
+module.exports = feeding

9557bca1c5fbccaade95e34acf3989e3a4ad314c
403-html <tymoteusz.stepien@wp.pl>
2020-11-28 13:04:26 +0100

    Fixed binary tree

--- binary-tree/tree.js
+++ binary-tree/tree.js
@@ -1,35 +1,60 @@
-class Node {
-    constructor(val, index) {
-        this.number = val || null;
-        this.left = null;
-        this.right = null;
-        this.index = index || null;
-    }
-}
-
 class Tree {
-    constructor() {
-        this.tree = new Node();
-    }
-    addValue({value,index}) {
-        if(index == 0){
-            this.tree.index = index;
-            this.tree.number = value;
-        }
-        else {
-            if(value < this.tree.number) this.tree.left = new Node(value,index);
-            if(value > this.tree.number) this.tree.right = new Node(value,index);
+    constructor() {}
+    addValue(value) {
+        if (!this.number) {
+            this.number = value
+            this.branchIndex = 0
+        } else {
+            if (this.valueIndex) this.valueIndex += 1
+            else this.valueIndex = 1
+            // right, higher
+            if (value > this.number) {
+                if (this.right) this.right.addValue(value, this.valueIndex)
+                if (!this.right) this.right = new Branch(value, this.valueIndex)
+            }
+            // left, lower
+            if (value < this.number) {
+                if (this.left) this.left.addValue(value, this.valueIndex)
+                if (!this.left) this.left = new Branch(value, this.valueIndex)
+            }
         }
     }
 }
 
-let tree = new Tree();
-
-for(let i = 0; i<15; i++){
-    tree.addValue({
-        "value": Math.floor(Math.random()*101),
-        "index": i
-    });
+class Branch {
+    constructor(value, index) {
+        this.number = value
+        this.branchIndex = index
+    }
+    addValue(value, index) {
+        // right, higher
+        if (value > this.number) {
+            if (!this.right) this.right = new Branch(value, index)
+            if (this.right) this.right.addValue(value, index)
+        }
+        // left, lower
+        if (value < this.number) {
+            if (!this.left) this.left = new Branch(value, index)
+            if (this.left) this.left.addValue(value, index)
+        }
+    }
 }
 
-console.log(tree.tree);
+/*
+ branch (nr, index)
+   -> inaczej
+        -> sprawdź czy pasuje do lewego brancha (mniejszy niż numer)
+            -> nie, to wstaw numerem i indexem
+            -> tak, to wrzuć nowy branch, który stworzy nowy branch
+        -> sprawdź czy pasuje do prawego brancha (wiekszy niż numer)
+            -> nie, to wstaw nowy branch z numerem
+            -> tak, to wrzuć nowy branch, który stworzy nowy branch
+*/
+
+// let tree = new Tree()
+
+// for (let i = 0; i < 15; i++) {
+//     tree.addValue(Math.floor(Math.random() * 101))
+// }
+
+// console.log(JSON.stringify(tree, null, 2))

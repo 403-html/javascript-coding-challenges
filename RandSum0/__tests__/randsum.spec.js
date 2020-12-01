ce1bb523118a2872bfa89d3198cdc12f3c68e7b9
403-html <tymoteusz.stepien@wp.pl>
2020-12-01 21:37:06 +0100

    RandSum blank file + fix tests

--- RandSum0/__tests__/randsum.spec.js
+++ RandSum0/__tests__/randsum.spec.js
@@ -1,35 +1,33 @@
 const genSum = require('../index')
 
-const sumArrElems = (arr) => arr.reduce((a, b) => a + b)
-
 test('Providing "0" as argument should give an array that\'s empty', () => {
     const arrayLength = 0
-    const randomArray = new genSum(arrayLength).array
+    const randomArray = new genSum(arrayLength)
 
-    expect(randomArray.length).toBe(arrayLength)
-    expect(randomArray).toEqual([])
+    expect(randomArray.array.length).toBe(arrayLength)
+    expect(randomArray.sumArray()).toEqual([])
 })
 
 test('Providing "1" as argument should give an array that contains "0"', () => {
     const arrayLength = 1
-    const randomArray = new genSum(arrayLength).array
+    const randomArray = new genSum(arrayLength)
 
-    expect(randomArray.length).toBe(arrayLength)
-    expect(randomArray[0]).toEqual(0)
+    expect(randomArray.array.length).toBe(arrayLength)
+    expect(randomArray.sumArray()).toEqual(0)
 })
 
 test('Array should have length 4, and values should sum to 0', () => {
     const arrayLength = 4
-    const randomArray = new genSum(arrayLength).array
+    const randomArray = new genSum(arrayLength)
 
-    expect(randomArray.length).toBe(arrayLength)
-    expect(sumArrElems(randomArray)).toEqual(0)
+    expect(randomArray.array.length).toBe(arrayLength)
+    expect(randomArray.sumArray()).toEqual(0)
 })
 
 test('Array should have length 17, and values should sum to 0', () => {
     const arrayLength = 17
-    const randomArray = new genSum(arrayLength).array
+    const randomArray = new genSum(arrayLength)
 
-    expect(randomArray.length).toBe(arrayLength)
-    expect(sumArrElems(randomArray)).toEqual(0)
+    expect(randomArray.array.length).toBe(arrayLength)
+    expect(randomArray.sumArray()).toEqual(0)
 })
---
+++ RandSum0/randsum.js
@@ -0,0 +1,11 @@
+/* Random numbers which always sum to 0 */
+
+class genSum {
+    constructor(n) {
+        this.array = []
+    }
+    sumArray() {}
+}
+
+// Don't delete export
+module.exports = genSum

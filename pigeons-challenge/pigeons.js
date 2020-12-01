f4f917c8ec4b9b3e8c3ecaf7594e6d8837a9ba76
403-html <tymoteusz.stepien@wp.pl>
2020-12-01 21:12:02 +0100

    Pigeons blank file

--- pigeons-challenge/pigeons.js
+++ pigeons-challenge/pigeons.js
@@ -1,14 +1,8 @@
-const feeding = (totalWheat = 0) => {
-    let pigeonsTotal = 0
-    let nextPigeonGroup = 1
-    let currWheat = totalWheat
-    while (pigeonsTotal < currWheat) {
-        if (pigeonsTotal + nextPigeonGroup > totalWheat) break
-        pigeonsTotal += nextPigeonGroup
-        currWheat -= pigeonsTotal
-        nextPigeonGroup++
-    }
-    return pigeonsTotal
+/* Feeding pigeons */
+
+const feeding = (wheat) => {
+    return totalPigeons
 }
 
+// Don't delete export
 module.exports = feeding

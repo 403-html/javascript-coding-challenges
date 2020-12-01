74ce35c15efde7aaee347f46d7d4ac2ae21f7950
403-html <tymoteusz.stepien@wp.pl>
2020-12-01 20:57:40 +0100

    FizzBuzz

---
+++ fizzbuzz/my_fizzbuzz_solution.js
@@ -0,0 +1,19 @@
+const isDividiedBy = (num, multiplier) => {
+    if (num % multiplier == 0) {
+        return true
+    }
+}
+
+const fizzbuzz = (n) => {
+    return new Array(n).fill().map((val, index) => {
+        if (isDividiedBy(index + 1, 3) && isDividiedBy(index + 1, 5)) {
+            return 'FizzBuzz'
+        } else if (isDividiedBy(index + 1, 3) || isDividiedBy(index + 1, 5)) {
+            if (isDividiedBy(index + 1, 3)) return 'Fizz'
+            if (isDividiedBy(index + 1, 5)) return 'Buzz'
+        }
+        return index + 1
+    })
+}
+
+module.exports = fizzbuzz
